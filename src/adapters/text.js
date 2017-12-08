bce46f718f2ee97f9d4cdf77d967ef1249b98270
Guillaume Ducret <guillaume@ducret201.com>
2017-12-08 08:46:39 +0100

    Add support for text rotation

--- src/adapters/text.js
+++ src/adapters/text.js
@@ -1,7 +1,7 @@
 import {applyDocProperties, revertDocProperties} from './adapter'
 
 export default function(element, context, next) {
-    const {children, x, y, width, height, gravity, ...props} = element.props
+    const {children, x, y, width, height, gravity, rotate = 0, ...props} = element.props
     const {doc, layout} = context
 
     doc.save()
@@ -15,6 +15,7 @@ export default function(element, context, next) {
 
     const position = layout({x, y, width, height: height || computeHeight, gravity})
 
+    doc.rotate(rotate, {origin: [position.x, position.y]})
     doc.text(childNodes.join(''), position.x, position.y, {width: position.width, ...props, ...position.option})
     doc.y = !position.height && doc.y || position.y
 
